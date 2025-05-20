035a1c0090c4c881950b08324de1f91f73381aae
sanjaysaini383 <sanjaysaini4423@gmail.com>
2025-05-20 14:05:16 +0530

    project complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,29 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-
+// import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthstore";
 
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthstore";
-// import { useThemeStore } from "./store/useThemeStore";
+
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import { useEffect } from "react";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  // const { theme } = useThemeStore();
+  const { theme } = useThemeStore();
 
   console.log({ onlineUsers });
+
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
   console.log({ authUser });
-  // console.log({ isCheckingAuth });
 
   if (isCheckingAuth && !authUser)
     return (
@@ -33,19 +34,20 @@ const App = () => {
       </div>
     );
 
-
-
   return (
     <div data-theme="dark">
       <Navbar />
+
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        {/* <Route path="/settings" element={<SettingsPage />} /> */}
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
       <Toaster />
     </div>
   );
-}
+};
 export default App;
